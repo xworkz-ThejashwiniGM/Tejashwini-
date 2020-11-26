4cd73211d0e251d70222744c4d3e53c1b4b3fe1e
xworkz-ThejashwiniGM <teju.xworkz@gmail.com>
2020-11-26 18:37:13 +0530

    adding wrapper java file

---
+++ wrapper/Avg.java
@@ -0,0 +1,9 @@
+
+public class FindAvg
+{
+static double avg(double firstsubject,double secondsubject,double thirdsubject)
+{
+double total=(firstsubject+secondsubject+thirdsubject)/3;
+return total;
+}
+}
