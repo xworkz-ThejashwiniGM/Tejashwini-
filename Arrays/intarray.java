2b8e160683b4c1c82630b00a273898b44d5d4f67
xworkz-ThejashwiniGM <teju.xworkz@gmail.com>
2020-11-26 23:18:23 +0530

    adding Arrays java file

---
+++ Arrays/intarray.java
@@ -0,0 +1,12 @@
+class NumberofTemples
+{
+public static void main(String a[])
+{
+int NumberofTemples[]={17,31,6,9,3};
+System.out.println(NumberofTemples[0]);
+System.out.println(NumberofTemples[1]);
+System.out.println(NumberofTemples[2]);
+System.out.println(NumberofTemples[3]);
+System.out.println(NumberofTemples[4]);
+}
+}
---
+++ Arrays/longarray.java
@@ -0,0 +1,9 @@
+class PhoneNumbers
+{
+public static void main(String a[])
+{
+long[] phonenumbers={123456l , 456789l, 789678l , 555555l , 34343434l};
+System.out.println(phonenumbers[0] +  "  " + phonenumbers[1] +  "  " + phonenumbers[2] + " "+ phonenumbers[3]);
+
+}
+}
