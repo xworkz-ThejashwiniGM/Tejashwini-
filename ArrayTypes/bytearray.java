7551206864c32d9ef546ef7663021ce7500dc0da
xworkz-ThejashwiniGM <teju.xworkz@gmail.com>
2020-11-29 19:21:34 +0530

    adding DataTypes files

---
+++ ArrayTypes/bytearray.java
@@ -0,0 +1,12 @@
+class Mentos
+{
+public static void main(String a[])
+{
+byte[] mentos={17,31,6,9,3};
+System.out.println(mentos[0]);
+System.out.println(mentos[1]);
+System.out.println(mentos[2]);
+System.out.println(mentos[3]);
+System.out.println(mentos[4]);
+}
+}
---
+++ ControlStatement/barfor.java
@@ -0,0 +1,9 @@
+class Bar
+{
+public static void main(String a[])
+{
+String mentos[]={"scotch","brandy","30","60","90","mocktail"};
+for(int mentosValue=0 ; mentosValue<=5; mentosValue++)
+System.out.println(mentos[mentosValue]);
+}
+}
