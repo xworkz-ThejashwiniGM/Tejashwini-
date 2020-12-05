9220d6ed8e74312b0190ea52ef077ca29330c0e3
xworkz-ThejashwiniGM <teju.xworkz@gmail.com>
2020-12-05 22:09:58 +0530

    adding java file

---
+++ stringex-proj/src/com/xworkz/StringEx.java
@@ -0,0 +1,26 @@
+package com.xworkz;
+
+public class StringEx {
+	public static void main(String[] args)
+	{
+	String	name=new String ("stories");//2 objects has been stored
+	System.out.println(name);
+	String name1=new String("janapada");
+	System.out.println(name1);
+	String name3=new String("nice dine");
+	System.out.println(name3);
+	String name4=new String ("shantisagar");
+	System.out.println(name4);
+	
+	
+	
+	
+	String name5="stories";//0 object
+	System.out.println(name5);
+		String name6="nice done";//0 object
+		System.out.println(name6);
+			String nam="stories";
+			System.out.println(nam);
+	}
+
+}
