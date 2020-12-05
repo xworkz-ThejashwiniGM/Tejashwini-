a172d930bd8e8e263e8047a07b9861bc9e89909e
xworkz-ThejashwiniGM <teju.xworkz@gmail.com>
2020-12-05 21:31:26 +0530

    adding java file

---
+++ bookshop-proj/src/com/xworkz/BookShopes.java
@@ -0,0 +1,13 @@
+package com.xworkz;
+
+public class BookShopes {
+	public String name;
+	public String address;
+	public int price;
+	public int noOfBooks;
+	public void toRead()
+	{
+	System.out.println("to give lot of books");
+	}
+	}
+
---
+++ bookshop-proj/src/com/xworkz/book/BookShopesUtil.java
@@ -0,0 +1,27 @@
+package com.xworkz.book;
+
+import com.xworkz.BookShopes;
+
+public class BookShopesUtil {
+
+	public static void main(String[] args) {
+		
+		BookShopes book=new BookShopes();
+		book.name="Tm";
+		book.address="Mejastic";
+		book.price=12;
+		book.noOfBooks=68;
+		book.toRead();
+		System.out.println(book.name);
+		System.out.println(book.address);
+		System.out.println(book.price);
+		System.out.println(book.noOfBooks);
+		}
+		
+
+
+		 
+
+	}
+
+
