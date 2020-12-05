de2f42e3042cd7bbd48583956685d6e7c0572393
xworkz-ThejashwiniGM <teju.xworkz@gmail.com>
2020-12-05 22:14:17 +0530

    adding java file

---
+++ stringobj-proj/src/com/xworkz/StringUtil.java
@@ -0,0 +1,41 @@
+package com.xworkz;
+
+public class StringUtil {
+public static void main(String[] args) {
+	
+	String s1="Aarti";
+	String s2="aarti";
+	//System.out.println(s1.equals (s2));
+	//System.out.println(s1==s2);
+	//System.out.println(s1.equalsIgnoreCase(s2));
+	//System.out.println(s1.matches(s2));
+	//System.out.println(s1.length());
+	//System.out.println(s2.concat("teju"));
+	//System.out.println(s1.indexOf("r"));
+
+
+	//String s3=new String("xworkz");
+	//String s4=new String("xworkz");
+	//System.out.println(s3==s4);
+
+	
+	String s5="     at   ";
+	//System.out.println(s1.contains(s5));
+	//System.out.println(s5.trim());
+	//System.out.println(s5.indexOf("t"));
+
+	
+	
+	//String s6=new String("teju uttu naaz");
+	//String s7="teju";
+	//System.out.println(s6.matches(s7));
+	
+	
+	String s8=new String("");
+	//System.out.println(s8.isEmpty());
+	String s9=new String("  ");
+	//System.out.println(s9.isEmpty());
+	
+
+}
+}
