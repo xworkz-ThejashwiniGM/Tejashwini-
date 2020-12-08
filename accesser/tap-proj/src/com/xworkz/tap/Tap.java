73111522a3285d7cc9684a1b726fc26f89e7c4e7
xworkz-ThejashwiniGM <teju.xworkz@gmail.com>
2020-12-08 12:20:06 +0530

    adding java file

---
+++ accesser/tap-proj/src/com/xworkz/tap/Tap.java
@@ -0,0 +1,27 @@
+package com.xworkz.tap;
+
+public class Tap {
+	private String name;
+	private int id;
+	private double cost;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public double getCost() {
+		return cost;
+	}
+	public void setCost(double cost) {
+		this.cost = cost;
+	}
+	
+
+}
---
+++ accesser/tap-proj/src/com/xworkz/util/TapUtil.java
@@ -0,0 +1,16 @@
+package com.xworkz.util;
+
+import com.xworkz.tap.Tap;
+
+public class TapUtil {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Tap tap = new Tap();
+tap.setName("Jaquar");
+tap.setId(25684);
+tap.setCost(450.2);
+System.out.println(tap.getName()+" "+tap.getId()+" "+tap.getCost()+" ");
+	}
+
+}
