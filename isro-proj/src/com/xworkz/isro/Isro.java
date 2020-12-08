e340203d755391996003bbbd718d20cdc6fc2239
xworkz-ThejashwiniGM <teju.xworkz@gmail.com>
2020-12-08 09:41:03 +0530

    adding java file

---
+++ isro-proj/src/com/xworkz/isro/Isro.java
@@ -0,0 +1,19 @@
+package com.xworkz.isro;
+
+public class Isro {
+	public String place;
+	public Isro()
+	{
+		System.out.println("hello isro");
+	}
+public void  setPlace( String place)
+{
+	this.place=place;
+}
+	public String getPlace()
+	{
+	return place;
+	}
+ 
+}
+
---
+++ isro-proj/src/com/xworkz/util/IsroUtil.java
@@ -0,0 +1,11 @@
+package com.xworkz.util;
+
+import com.xworkz.isro.Isro;
+
+public class IsroUtil {
+	public static void main(String[] args) {
+		Isro isro=new Isro();
+		isro.setPlace("Banglore");
+		System.out.println(isro.getPlace());
+	}
+}
