af6dce8d3c039c85463da774ec7ac1f80181c481
xworkz-ThejashwiniGM <teju.xworkz@gmail.com>
2020-12-23 23:50:16 +0530

    adding java file

---
+++ using-block-initialisation/block-proj/src/com/xworkz/BlockExProg.java
@@ -0,0 +1,56 @@
+package com.xworkz;
+
+public class BlockExProg 
+{
+	
+	public static  void main(String args[])//block statements
+	{
+		System.out.println("hii");
+		BlockExProg  bk=new BlockExProg();
+
+	}
+	{
+		//System.out.println("good morning");
+	}
+	{
+		//System.out.println("good Afternoon");
+	}
+	{
+		//System.out.println("good evening");
+	}
+	{
+	
+		System.out.println("good night");
+	}
+	
+		
+	
+	{     //if u use static keyword first excute the static statement and then block statement and then main
+	int a=10;
+	int b=20;
+	int c=a+b;
+		
+	System.out.println(c);
+	{
+		int d=10;
+		int e=30;
+		int f=d+e;
+		System.out.println(f);
+	}
+	
+		
+	}
+	//static{
+	//	int d=10;
+	//	int e=60;
+		//int f=d+e;
+		
+		//System.out.println(f);
+
+	//}
+
+		
+}	
+
+
+
